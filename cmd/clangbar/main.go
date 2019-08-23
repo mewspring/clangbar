7412149014b39ffc157f2ed392d59ed980e1edee
Robin Eklind <rnd0x00@gmail.com>
2019-08-23 17:53:33 +0200

    cmd/clangbar: resolve external global variable definitions

--- cmd/clangbar/main.go
+++ cmd/clangbar/main.go
@@ -47,16 +47,54 @@ func analyze(root *cc.Node) {
 	for _, f := range funcs {
 		pretty.Printf("   %v (at %q)\n", f.Body.Spelling(), f.Loc)
 	}
+	var funcUses []*FuncUse
 	for _, f := range funcs {
 		uses := findUses(f)
+		funcUse := &FuncUse{
+			fn:   f,
+			uses: uses,
+		}
+		funcUses = append(funcUses, funcUse)
 		fmt.Printf("uses in function %v (at %q)\n", f.Body.Spelling(), f.Loc)
 		pretty.Println(uses)
 	}
+	globalDefs := make(map[string]*cc.Node)
+	for _, global := range globals {
+		name := global.Body.Spelling()
+		if old, ok := globalDefs[name]; ok {
+			panic(fmt.Errorf("global variable %q already present; old %v, new %v", name, old, global))
+		}
+		globalDefs[name] = global
+	}
+	for _, funcUse := range funcUses {
+		resolveExternalDefs(funcUse, globalDefs)
+	}
+	pretty.Println(funcUses)
 }
 
-func findUses(f *cc.Node) []Use {
+func resolveExternalDefs(funcUse *FuncUse, globalDefs map[string]*cc.Node) {
+	zero := cc.Location{}
+	for _, use := range funcUse.uses {
+		if use.DefLoc != zero {
+			// Already has location of definition.
+			continue
+		}
+		global, ok := globalDefs[use.Name]
+		if !ok {
+			warn.Printf("unable to resolve use of external global variable %q, as used in function %q (at %q)", use.Name, funcUse.fn.Body.Spelling(), funcUse.fn.Loc)
+		}
+		use.DefLoc = global.Loc
+	}
+}
+
+type FuncUse struct {
+	fn   *cc.Node
+	uses []*Use
+}
+
+func findUses(f *cc.Node) []*Use {
 	fmt.Printf("uses in function %v (at %q)\n", f.Body.Spelling(), f.Loc)
-	var uses []Use
+	var uses []*Use
 	cc.Walk(f, func(n *cc.Node) {
 		if n.Body.Kind() != clang.Cursor_DeclRefExpr {
 			// Early return if not identifier use expression.
@@ -65,7 +103,7 @@ func findUses(f *cc.Node) []Use {
 		if isExternal(n.Body.Definition()) {
 			fmt.Printf("   external use: %v (at %q)\n", n.Body.Spelling(), n.Loc)
 			// (external) global use.
-			use := Use{
+			use := &Use{
 				Name:   n.Body.Spelling(),
 				UseLoc: n.Loc,
 				// Note: external use has no definition, thus no DefLoc.
@@ -75,7 +113,7 @@ func findUses(f *cc.Node) []Use {
 		} else if isGlobal(n.Body.Definition()) {
 			fmt.Printf("   global use: %v (at %q)\n", n.Body.Spelling(), n.Loc)
 			// global use.
-			use := Use{
+			use := &Use{
 				Name:   n.Body.Spelling(),
 				UseLoc: n.Loc,
 				DefLoc: cc.NewLocation(n.Body.Definition().Location()),
